9073db5cbc88d60245a6f3b1d6fdb13026c5511a
Jacob <87475035+Jang-jhih@users.noreply.github.com>
2022-10-17 14:23:13 +0800

    test

--- 1.建立調閱路徑/document_retrieval.py
+++ 1.建立調閱路徑/document_retrieval.py
@@ -6,5 +6,6 @@ from condition.Condition import *
 # Auto_mkdir(file = '調閱清單111.9.15_行政部.xlsx', test = False,creattxt = False)
 # Auto_mkdir(file = '調閱清單111.9.15_行銷部.xlsx', test = False,creattxt = False)
 # Auto_mkdir(file = '調閱清單111.9.22_風險管理部.xlsx', test = False,creattxt = False)
+Auto_mkdir(file = '調閱清單111.10.XX_業務部.xlsx', test = False,creattxt = False)
 
-del_none_folder(r'D:\OneDrive - 全支付電子支付股份有限公司\1.建立調閱路徑\test')
+del_none_folder(path = r'D:\OneDrive - 全支付電子支付股份有限公司\Audit_RPA\1.建立調閱路徑\test')
